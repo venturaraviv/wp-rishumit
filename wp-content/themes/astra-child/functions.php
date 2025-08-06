1ed6d002eae95b9b1e87467f3c8c6da2a2197c80
Jacob Orbach <jorbach@one-digital.co.il>
2025-08-06 13:06:33 +0300

    added missing ga4 tag

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -10,11 +10,14 @@ function rishumit_add_gtag_script()
     ?>
     <!-- Global site tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16765175858"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NWX5NEWFM4"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
-      function gtag() { dataLayer.push(arguments); }
+      function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
+
       gtag('config', 'AW-16765175858');
+      gtag('config', 'G-NWX5NEWFM4');
     </script>
     <?php
 }
